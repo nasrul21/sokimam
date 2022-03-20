03d0cc549a17737f352e1db8c23c0c8ca18d3d21
Nasrul Faizin <faizinnasrul21@gmail.com>
2022-03-20 08:47:29 +0700

    feat(user): endpoint register user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:6',
+            'name' => 'required|min:3'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), Response::HTTP_OK);
+        }
+
+        try {
+            $user = User::create($request->all());
+
+            $response = [
+                "message" => "Transaction created",
+                "data" => $user,
+            ];
+
+            return response()->json($response, Response::HTTP_CREATED);
+        } catch (QueryException $e) {
+            return response()->json([
+                "message" => $e->getMessage(),
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+
+
+        return response()->json(['message' => 'Data added successfully'], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post("/register", [UserController::class, 'register']);
+Route::post("/register", [UserController::class, 'register']);
